babb076bcde0ce7287fbe867919e59baf765c95a
unknown <Saitharala@gmail.com>
2021-12-04 00:42:19 +0530

    [TSP] Added Collections Feauture

---
+++ Collections/Collections/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Collections Of :");
+            Console.WriteLine("\nList");
+            Operations.ListDemo();
+            Console.WriteLine("\nStack");
+            Operations.StackDemo();
+            Console.WriteLine("\nQueue");
+            Operations.QueueDemo();
+            Console.WriteLine("\nHashset");
+            Operations.HashSetDemo();
+            Console.WriteLine("\nDictionary");
+            Operations.DictionaryDemo();
+        }
+    }
+}
